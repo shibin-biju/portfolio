3f59c03687bebbdda516ecb0d8a7d9e6daeeb702
shibin-biju <shibinbiju706@gmail.com>
2025-08-11 14:48:42 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,36 +152,16 @@ themeButton.addEventListener('click', () => {
 
 
 function getMessage(){
-    const BOT_TOKEN = "7294030355:AAG1KYf0RKtfaoq4iENzHZh3ZXNDrW7eGEY";
-    const CHAT_ID = "1130642998";
-    const TELEGRAM_URL = `https://api.telegram.org/bot7294030355:AAG1KYf0RKtfaoq4iENzHZh3ZXNDrW7eGEY/sendMessage`;
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyjFKfRgYJBYZGD1Esl1v92i_tyW-Z4K6x1X6HCuiuxg81aORLspgzU1vS0o0UqQJFy/exec'
+const form = document.forms['contact-me']
 
-    document.getElementById("contactForm").addEventListener("submit", function(e) {
-      e.preventDefault(); // prevent default form submit
-
-      const formData = new FormData(this);
-      let message = `📩 *New Form Submission:*\n`;
-      formData.forEach((value, key) => {
-        message += `*${key}*: ${value}\n`;
-      });
-
-      fetch(TELEGRAM_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          chat_id: CHAT_ID,
-          text: message,
-          parse_mode: "Markdown"
-        })
-      })
-      .then(res => res.json())
-      .then(data => {
-        alert("✅ Message sent to Telegram!");
-        this.reset();
-      })
-      .catch(err => {
-        alert("❌ Error sending message.");
-        console.error(err);
-      });
-    });
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{ 
+        form.reset()
+        alert('Message Send!')
+    })
+    .catch(error => console.error('Error!', error.message))
+})
 }
